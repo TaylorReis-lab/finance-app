23e0ff78606e9104958cb65045eb3d42e22af16c
Taylor Reis <reist98822@gmail.com>
2025-05-03 22:17:25 -0300

    edited: name for this app

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
   return (
     <Box sx={{ p: 3 }}>
       <Typography variant="h4" gutterBottom>
-        Dashboard Financeiro
+        Overview
       </Typography>
       
       <Suspense fallback={<CircularProgress />}>
